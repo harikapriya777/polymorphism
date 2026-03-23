d91b5194b485a2c9dd2b25a13dcb280a3fb15889
harikapriya777 <harikapriyakurapati@gmail.com>
2026-03-23 17:58:34 +0530

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,23 @@
+#polymorphism
+#operator overloading
+'''a=2;b=4
+print(a+b)
+print(a.__add__(b))
+print(a.__add__(6))
+print(a.__sub__(1))
+print(a.__mul__(5))
+#print(a.__div__(2))
+print(a.__pow__(2))
+print(a.__le__(8))#le means lessthan
+print(a.__ge__(3))#ge means greater than
+a=[1,2,3,4,5,6,7];b=[6,7,8,9,10,11]
+print(a.__add__(b))
+print(a.__getitem__(3))
+print(b.__getitem__(5))
+a="code";b="gnan"
+print(a.__add__(b))
+a="python";b="course"
+print(a.__add__(" "+b))
+print("harika".__add__(" "+"k").title())'''
+
+
